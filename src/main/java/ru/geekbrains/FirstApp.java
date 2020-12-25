ed1fc4d8a1b4d7c3c2d2f4149b0f99f321f7c139
artemternovenko <artem.inkaz@gmail.com>
2020-12-25 18:17:07 +0600

    Initial commit

---
+++ src/main/java/ru/geekbrains/FirstApp.java
@@ -0,0 +1,12 @@
+package ru.geekbrains;
+
+public class FirstApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello GeekBrains");
+
+        boolean bool=true;
+        float f= 1.23f
+}
+}
