15f22eb35abc78085848810838c114b709bf4383
artemternovenko <artem.inkaz@gmail.com>
2021-02-04 12:13:06 +0600

    Lessons #9 update #2 04022021

--- src/main/resources/example.sql
+++ src/main/resources/example.sql
@@ -8,11 +8,13 @@ CREATE TABLE IF NOT EXISTS Cats
 
 CREATE TABLE Cats(CatID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Name TEXT NOT NULL, Age INTEGER NOT NULL, Type TEXT NOT NULL, Sex TEXT NOT NULL );
 
-insert into Cats(Name, Age, Type, Sex) values ('Barsik', 2, Bobtail, M), ('Izolda', 1, Siam, W), ('Dymok', 3, Homeless,M);
+insert into Cats(Name, Age, Type, Sex) values ('Barsik', 2, 'Bobtail', 'M'), ('Izolda', 1, 'Siam', 'W'), ('Dymok', 3, 'Homeless','M');
 
 select * from Cats;
 
-delete from Cats where Name like '%Bob%';
+delete from Cats where Type like '%Home%';
 delete from Cats where CatID > 100;
+delete from Cats where id = ?;
 
 update Cats set Name = 'Tom' where CatID < 100;
+update Cats set Name = 'Jungle' where CatID = ?;
